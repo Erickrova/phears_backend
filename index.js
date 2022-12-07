cdac505c562d4dee59cd708b7ebc614c7798b286
Erickrova <crachipp01@gmail.com>
2022-12-06 23:48:13 -0500

    1.0.1

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const app = express()
 dotenv.config()
 connectDB()
 const whiteList = [process.env.FRONTEND_URL]
-app.use(cors({origin:whiteList}))
+const corsOptions = {
+    origin: function(origin,callback){
+        if(whiteList.includes(origin)){
+            // puede consultar la API
+            callback(null,true)
+        }else{
+            // no esta permitido su request
+            callback(new Error("Error de cors"))
+        }
+    }
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 
 
